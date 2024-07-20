ecad29c85660488f6f08b412f6ef9fef2ec28a39
Karl Cini <156437065+outatime75@users.noreply.github.com>
2024-07-20 14:05:05 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,9 @@
+'''
+N. Krishna, “Faster R-CNN: A Step by Step Guide,” Towards Data Science, 2 November 2022. [Online]. 
+Available: https://towardsdatascience.com/understanding-and-implementing-faster-r-cnn-a-step-by-step-guide-11acfff216b0. [Accessed 3 January 2023].
+'''
+
+
 import torch
 import torchvision
 from torchvision import ops
